bb8d8ac5aa5bed76c8ef3e8da9e8bda515c6e8fd
Sunil Umesh <sunil_u123@hotmail.com>
2020-07-26 18:30:32 +1000

    Update Fluo-N2DL-HeLa_cell_segmentation.py

--- Fluo-N2DL-HeLa_cell_segmentation.py
+++ Fluo-N2DL-HeLa_cell_segmentation.py
@@ -17,20 +17,12 @@ while sequence.isOpened():
         break
     image_copy = image.copy()
     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-   
+       
     #Background Subtraction
     img_min = cv2.erode(img, kernel)
     img_max = cv2.dilate(img_min, kernel)
     img_bg = cv2.subtract(img,img_max)
     
-
-##    img_max = cv2.dilate(img, kernel)    
-##    img_min = cv2.erode(img_max, kernel)
-##    img_bg = cv2.subtract(img,img_min)
-    
-    
-    #cv2.imshow('Image',img_bg)
     #Smoothing and thresholding
     gaussian = cv2.GaussianBlur(img_bg, (7,7), 0)
     _, thres = cv2.threshold(gaussian,0,255,cv2.THRESH_BINARY +cv2.THRESH_OTSU)
@@ -54,22 +46,21 @@ while sequence.isOpened():
     counter = 0
     box = []
     for i in range(len(contours)):
-        contours2 = cv2.findContours(canny_output, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        contour2 = imutils.grab_contours(contours2)
+        if cv2.contourArea(contours[i]) < 200:
+            continue
         centre = None
-        contour_max = max(contour, key=cv2.contourArea)
+        contour_max = max(contours, key=cv2.contourArea)
         (x, y, w, h) = cv2.boundingRect(contour_max)
         M = cv2.moments(contour_max)
         if M["m00"] and M["m00"]:
             centre = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            box.append(centre)
+            if centre not in box:
+                box.append(centre)
         color = (255, 255, 0)
         cv2.rectangle(dilate, (int(boundRect[i][0]), int(boundRect[i][1])),(int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
         counter += 1
-
     cv2.putText(dilate, "Count: {}".format(counter), (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (255, 0, 0), 2)
-        
+                    1, (255, 0, 0), 2)    
     cv2.imshow('Fluo-N2DL-HeLa', dilate)
 
     
